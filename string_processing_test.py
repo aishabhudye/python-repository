8edf81a6a7061a6175b1c07d238922f0451228eb
zulbhudy <zabdsb@gmail.com>
2022-07-31 15:34:11 +0100

    Fix typo

--- string_processing_test.py
+++ string_processing_test.py
@@ -5,12 +5,12 @@ from string_processing import StringProcessor
 
 class MyTestCase(unittest.TestCase):
     def setUp(self) -> None:
-        self.test_targettarget = StringProcessor()
+        self.test_target = StringProcessor()
 
     def test_reverse(self):
         sibling_array = ["Aisha", "Hafsa", "Talha"]
         sibling_array_in_reverse = ["Talha", "Hafsa", "Aisha"]
-        self.assertEqual(self.test_targettarget.reverse_array(sibling_array), sibling_array_in_reverse)
+        self.assertEqual(self.test_target.reverse_array(sibling_array), sibling_array_in_reverse)
 
 
 if __name__ == '__main__':
