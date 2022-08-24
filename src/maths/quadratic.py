9fe1780f1bd1e1047f232cff9013a94524048888
Aisha <aisha.bhudye@gmail.com>
2022-08-24 17:26:56 +0100

    Quadratics using discriminants

--- src/maths/quadratic.py
+++ src/maths/quadratic.py
@@ -1,4 +1,16 @@
 class MathsProcessor:
 
     def solve(self, a, b, c):
-        return ""
+        a
+        b
+        c
+
+        discriminant = (b ** 2) - (4 * a * c)
+        if discriminant > 0:
+            return "2 distinct real roots"
+        elif discriminant == 0:
+            return "single real root"
+        elif discriminant < 0:
+            return "no real roots"
+
+
