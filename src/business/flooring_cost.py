bc8de57084973698401646a8154f93c580abbf6b
zulbhudy <zabdsb@gmail.com>
2022-09-07 11:24:41 +0100

    Refactored function to be testable and added a test case

--- src/business/flooring_cost.py
+++ src/business/flooring_cost.py
@@ -1,16 +1,20 @@
 class FlooringCost:
 
-    def calculate_cost(self):
-        total_cost = 0.0
+    def calculate_cost_given_inputs(self):
         length = input("Enter the length of your floor.")
         width = input("Enter the width of your floor")
+        return self.calculate_cost(length, width)
+
+    def calculate_cost(self, length, width):
         if length <= 0:
             print("ERROR")
+            total_cost = -1.0
         elif width > 4:
             print("ERROR")
+            total_cost = -1.0
         else:
             total_cost = length * width * 10
-            print('The total cost of installing carpet in a room of length {} and width {} is {}'.format(total_cost))
+            print('The total cost of installing carpet in a room of length {} and width {} is {}'.format(length, width, total_cost))
         return total_cost
 
 
--- tests/business/test_flooring_cost.py
+++ tests/business/test_flooring_cost.py
@@ -1,12 +1,21 @@
 from unittest import TestCase
 
+from src.business.flooring_cost import FlooringCost
+
 
 class TestFlooringCost(TestCase):
 
-    def test_print_total_cost_information(self):
-        self.test_target.print_length_and_width_and_total_cost_information()
-        pass
+    def setUp(self) -> None:
+        self.test_target = FlooringCost()
+    """
+    Positive test case: Max Length of room = 3.0m, Width of room = 4.0m. Therefore cost will be £120.00
+    """
+    def test_positive_scenario_1(self):
+        self.assertEqual(120.00, self.test_target.calculate_cost(3.0, 4.0))
 
-    def test_calculate_cost(self):
-        self.assertEqual(self.test_target.calculate_total())
+    """
+    Positive test case: Max Length of room = 3.0m, Width of room = 4.0m. Therefore cost will be £120.00
+    """
+    def test_positive_scenario_1(self):
+        self.assertEqual(120.00, self.test_target.calculate_cost(3.0, 4.0))
 
