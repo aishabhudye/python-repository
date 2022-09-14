e172ea7a4160d9b9cace74dccb1e9458a171e4cd
Aisha <aisha.bhudye@gmail.com>
2022-09-14 13:12:57 +0100

    tic-tac-toe

--- src/games/tic_tac_toe.py
+++ src/games/tic_tac_toe.py
@@ -4,19 +4,33 @@ class TicTacToe:
         print('Please specify which position/location you want your item to be as follows (0:X or 8:O)')
         print('The positions themselves are as follows')
         tic_tac_toe_locations = ["0", "1", "2", "3", "4", "5", "6", "7", "8"]
-        self.print_noughts_crosses_on_board(tic_tac_toe_locations)
+        self.print_board(tic_tac_toe_locations)
         noughts_crosses_array = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
         while self.board_is_not_full(noughts_crosses_array):
             location_value = input(
                 'Please specify which position/location you want your item to be as follows (0:X or 8:O) -> ')
-            input_array = location_value.split(":")
-            location = int(input_array[0])
-            value_at_location = input_array[1]
+            location, value_at_location = self.verify_input(location_value)
             noughts_crosses_array[location] = value_at_location
-            self.print_noughts_crosses_on_board(noughts_crosses_array)
+            self.print_board(noughts_crosses_array)
+
+    """Here we need to validate the location/address from 0 to 8 - the pigeon hole name"""
+    """Here we need to validate the value at the location/address from 0 to 8 - the pigeon hole content either 0 or X"""
+
+    def verify_input(self, location_value):
+        input_array = location_value.split(":")
+        location = int(input_array[0])
+        value_at_location = input_array[1]
+        X = input_array
+        """If the input is invalid in anyway, return a special flag/value"""
+        location_is_valid = (0 <= location <= 8)
+        value_is_valid = value_at_location == 'X' or value_at_location == "0"
+        if location_is_valid and value_is_valid:
+            return location, value_at_location
+        else:
+            return -1, "!"
 
     @staticmethod
-    def print_noughts_crosses_on_board(array_parameter):
+    def print_board(array_parameter):
         print('|{}|{}|{}|'.format(array_parameter[0], array_parameter[1], array_parameter[2]))
         print('|{}|{}|{}|'.format(array_parameter[3], array_parameter[4], array_parameter[5]))
         print('|{}|{}|{}|'.format(array_parameter[6], array_parameter[7], array_parameter[8]))
--- tests/games/test_tic_tac_toe.py
+++ tests/games/test_tic_tac_toe.py
@@ -25,6 +25,21 @@ class GamesTestCase(TestCase):
         noughts_crosses_array = ["X", "0", "X", "0", "X", "0", "X", "0", "X"]
         self.assertTrue(self.test_target.board_is_full(noughts_crosses_array), "Scenario 4")
 
+    def test_verify_input_scenario1(self):
+        self.assertEqual((0, 'X'), self.test_target.verify_input("0:X"), "verify_input_scenario1: +ve")
+
+    def test_verify_input_scenario2(self):
+        self.assertEqual((8, '0'), self.test_target.verify_input("8:0"), "verify_input_scenario2: +ve")
+
+    def test_verify_input_scenario3(self):
+        self.assertEqual((-1, '!'), self.test_target.verify_input("9:X"), "verify_input_scenario3: -ve")
+
+    def test_verify_input_scenario4(self):
+        self.assertEqual((-1, '!'), self.test_target.verify_input("8:Y"), "verify_input_scenario4: -ve")
+
+    def test_verify_input_scenario5(self):
+        self.assertEqual((-1, '!'), self.test_target.verify_input("9:Z"), "verify_input_scenario5: -ve")
+
 
 if __name__ == '__main__':
     unittest.main()
