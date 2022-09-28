a0a52e59add63c768185c89730833d56e8b0d0d1
Aisha <aisha.bhudye@gmail.com>
2022-09-28 11:05:54 +0100

    Hopefully a final version of my tic-tac-toe game until someone breaks

--- src/games/tic_tac_toe.py
+++ src/games/tic_tac_toe.py
@@ -6,17 +6,96 @@ class TicTacToe:
         tic_tac_toe_locations = ["0", "1", "2", "3", "4", "5", "6", "7", "8"]
         self.print_board(tic_tac_toe_locations)
         noughts_crosses_array = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+        previous_location = -1
+        previous_value = ''
         while self.board_is_not_full(noughts_crosses_array):
-            location_value = input(
+            user_input = input(
                 'Please specify which position/location you want your item to be as follows (0:X or 8:O) -> ')
-            location, value_at_location = self.verify_input(location_value)
-            noughts_crosses_array[location] = value_at_location
+            current_location, current_value = self.validate_input(user_input)
+            '''If the user input is correct, carry on, else prompt for input'''
+            user_input_is_invalid = current_location == -1 and current_value == '!'
+            duplicate_attempt = False
+            # print('CURRENT:{}->{} ; PREVIOUS:{}->{}'.format(current_location, current_value, previous_location,
+            #                                                 previous_value))
+            if previous_value == current_value or previous_location == current_location:
+                duplicate_attempt = True
+            while user_input_is_invalid or duplicate_attempt:
+                user_input = input(
+                    'Please specify which position/location you want your item to be as follows (0:X or 8:O) -> ')
+                current_location, current_value = self.validate_input(user_input)
+                duplicate_attempt = False
+
+            '''Store current value in previous value for comparison in the next iteration'''
+            previous_location = current_location
+            previous_value = current_value
+            noughts_crosses_array[current_location] = current_value
             self.print_board(noughts_crosses_array)
+
+            '''We need to check whether X or 0 has won at this point'''
+            if self.someone_has_won(noughts_crosses_array):
+                print('{} has won'.format(current_value))
+                break
+
+    '''
+    This returns a boolean flag either True or False
+    '''
+    def someone_has_won(self, noughts_crosses_array):
+        win_in_a_row = self.someone_has_won_in_a_row(noughts_crosses_array)
+        win_in_a_column = self.someone_has_won_in_a_column(noughts_crosses_array)
+        win_in_diagonal = self.someone_has_won_in_diagonal(noughts_crosses_array)
+        if win_in_a_row or win_in_a_column or win_in_diagonal:
+            return True
+        else:
+            return False
+
+    def someone_has_won_in_diagonal(self, noughts_crosses_array):
+        win = False
+        if noughts_crosses_array[0] == '0' and noughts_crosses_array[4] == '0' and noughts_crosses_array[8] == '0':
+            win = True
+        if noughts_crosses_array[0] == 'X' and noughts_crosses_array[4] == 'X' and noughts_crosses_array[8] == 'X':
+            win = True
+        if noughts_crosses_array[2] == '0' and noughts_crosses_array[4] == '0' and noughts_crosses_array[6] == '0':
+            win = True
+        if noughts_crosses_array[2] == 'X' and noughts_crosses_array[4] == 'X' and noughts_crosses_array[6] == 'X':
+            win = True
+        return win
+
+    def someone_has_won_in_a_column(self, noughts_crosses_array):
+        win = False
+        if noughts_crosses_array[0] == '0' and noughts_crosses_array[3] == '0' and noughts_crosses_array[6] == '0':
+            win = True
+        if noughts_crosses_array[0] == 'X' and noughts_crosses_array[3] == 'X' and noughts_crosses_array[6] == 'X':
+            win = True
+        if noughts_crosses_array[1] == '0' and noughts_crosses_array[4] == '0' and noughts_crosses_array[7] == '0':
+            win = True
+        if noughts_crosses_array[1] == 'X' and noughts_crosses_array[4] == 'X' and noughts_crosses_array[7] == 'X':
+            win = True
+        if noughts_crosses_array[2] == '0' and noughts_crosses_array[5] == '0' and noughts_crosses_array[8] == '0':
+            win = True
+        if noughts_crosses_array[2] == 'X' and noughts_crosses_array[5] == 'X' and noughts_crosses_array[8] == 'X':
+            win = True
+        return win
+
+    def someone_has_won_in_a_row(self, noughts_crosses_array):
+        win = False
+        if noughts_crosses_array[0] == '0' and noughts_crosses_array[1] == '0' and noughts_crosses_array[2] == '0':
+            win = True
+        if noughts_crosses_array[0] == 'X' and noughts_crosses_array[1] == 'X' and noughts_crosses_array[2] == 'X':
+            win = True
+        if noughts_crosses_array[3] == '0' and noughts_crosses_array[4] == '0' and noughts_crosses_array[5] == '0':
+            win = True
+        if noughts_crosses_array[3] == 'X' and noughts_crosses_array[4] == 'X' and noughts_crosses_array[5] == 'X':
+            win = True
+        if noughts_crosses_array[6] == '0' and noughts_crosses_array[7] == '0' and noughts_crosses_array[8] == '0':
+            win = True
+        if noughts_crosses_array[6] == 'X' and noughts_crosses_array[7] == 'X' and noughts_crosses_array[8] == 'X':
+            win = True
+        return win
 
     """Here we need to validate the location/address from 0 to 8 - the pigeon hole name"""
     """Here we need to validate the value at the location/address from 0 to 8 - the pigeon hole content either 0 or X"""
 
-    def verify_input(self, location_value):
+    def validate_input(self, location_value):
         input_array = location_value.split(":")
         location = int(input_array[0])
         value_at_location = input_array[1]
@@ -27,7 +106,8 @@ class TicTacToe:
         if location_is_valid and value_is_valid:
             return location, value_at_location
         else:
-            return -1, "!"
+            """The tuple (1, """
+            return -1, '!'
 
     @staticmethod
     def print_board(array_parameter):
@@ -40,7 +120,7 @@ class TicTacToe:
         populated_location_count = 0
         empty_location_count = 0
         for item in array_parameter:
-            if item == "X" or item == "0":
+            if item == 'X' or item == '0':
                 populated_location_count += 1
             else:
                 empty_location_count += 1
--- tests/games/test_tic_tac_toe.py
+++ tests/games/test_tic_tac_toe.py
@@ -26,19 +26,87 @@ class GamesTestCase(TestCase):
         self.assertTrue(self.test_target.board_is_full(noughts_crosses_array), "Scenario 4")
 
     def test_verify_input_scenario1(self):
-        self.assertEqual((0, 'X'), self.test_target.verify_input("0:X"), "verify_input_scenario1: +ve")
+        self.assertEqual((0, 'X'), self.test_target.validate_input("0:X"), "verify_input_scenario1: +ve")
 
     def test_verify_input_scenario2(self):
-        self.assertEqual((8, '0'), self.test_target.verify_input("8:0"), "verify_input_scenario2: +ve")
+        self.assertEqual((8, '0'), self.test_target.validate_input("8:0"), "verify_input_scenario2: +ve")
 
     def test_verify_input_scenario3(self):
-        self.assertEqual((-1, '!'), self.test_target.verify_input("9:X"), "verify_input_scenario3: -ve")
+        self.assertEqual((-1, '!'), self.test_target.validate_input("9:X"), "verify_input_scenario3: -ve")
 
     def test_verify_input_scenario4(self):
-        self.assertEqual((-1, '!'), self.test_target.verify_input("8:Y"), "verify_input_scenario4: -ve")
+        self.assertEqual((-1, '!'), self.test_target.validate_input("8:Y"), "verify_input_scenario4: -ve")
 
     def test_verify_input_scenario5(self):
-        self.assertEqual((-1, '!'), self.test_target.verify_input("9:Z"), "verify_input_scenario5: -ve")
+        self.assertEqual((-1, '!'), self.test_target.validate_input("9:Z"), "verify_input_scenario5: -ve")
+
+    def test_nought_has_won_scenario1(self):
+        noughts_crosses_array = ["0", "0", "0", " ", " ", " ", " ", " ", " "]
+        self.assertTrue(self.test_target.someone_has_won(noughts_crosses_array))
+
+    def test_nought_has_won_scenario2(self):
+        noughts_crosses_array = ["X", "X", "X", " ", " ", " ", " ", " ", " "]
+        self.assertTrue(self.test_target.someone_has_won(noughts_crosses_array))
+
+    def test_nought_has_won_scenario3(self):
+        noughts_crosses_array = ["", "", "", "0", "0", "0", " ", " ", " "]
+        self.assertTrue(self.test_target.someone_has_won(noughts_crosses_array))
+
+    def test_nought_has_won_scenario4(self):
+        noughts_crosses_array = ["", "", "", "X", "X", "X", " ", " ", " "]
+        self.assertTrue(self.test_target.someone_has_won(noughts_crosses_array))
+
+    def test_nought_has_won_scenario5(self):
+        noughts_crosses_array = ["", "", "", "", "", "", "0", "0", "0"]
+        self.assertTrue(self.test_target.someone_has_won(noughts_crosses_array))
+
+    def test_nought_has_won_scenario6(self):
+        noughts_crosses_array = ["", "", "", "", "", "", "X", "X", "X"]
+        self.assertTrue(self.test_target.someone_has_won(noughts_crosses_array))
+
+    def test_nought_has_won_scenario7(self):
+        noughts_crosses_array = ["0", "", "", "0", "", "", "0", "", ""]
+        self.assertTrue(self.test_target.someone_has_won(noughts_crosses_array))
+
+    def test_nought_has_won_scenario8(self):
+        noughts_crosses_array = ["X", "", "", "X", "", "", "X", "", ""]
+        self.assertTrue(self.test_target.someone_has_won(noughts_crosses_array))
+
+    def test_nought_has_won_scenario9(self):
+        noughts_crosses_array = ["", "0", "", "", "0", "", "", "0", ""]
+        self.assertTrue(self.test_target.someone_has_won(noughts_crosses_array))
+
+    def test_nought_has_won_scenario10(self):
+        noughts_crosses_array = ["", "X", "", "", "X", "", "", "X", ""]
+        self.assertTrue(self.test_target.someone_has_won(noughts_crosses_array))
+
+    def test_nought_has_won_scenario11(self):
+        noughts_crosses_array = ["", "", "0", "", "", "0", "", "", "0"]
+        self.assertTrue(self.test_target.someone_has_won(noughts_crosses_array))
+
+    def test_nought_has_won_scenario12(self):
+        noughts_crosses_array = ["", "", "X", "", "", "X", "", "", "X"]
+        self.assertTrue(self.test_target.someone_has_won(noughts_crosses_array))
+
+    def test_nought_has_won_scenario13(self):
+        noughts_crosses_array = ["0", "", "", "", "0", "", "", "", "0"]
+        self.assertTrue(self.test_target.someone_has_won(noughts_crosses_array))
+
+    def test_nought_has_won_scenario14(self):
+        noughts_crosses_array = ["X", "", "", "", "X", "", "", "", "X"]
+        self.assertTrue(self.test_target.someone_has_won(noughts_crosses_array))
+
+    def test_nought_has_won_scenario15(self):
+        noughts_crosses_array = ["", "", "0", "", "0", "", "0", "", ""]
+        self.assertTrue(self.test_target.someone_has_won(noughts_crosses_array))
+
+    def test_nought_has_won_scenario16(self):
+        noughts_crosses_array = ["", "", "X", "", "X", "", "X", "", ""]
+        self.assertTrue(self.test_target.someone_has_won(noughts_crosses_array))
+
+    def test_bug_scenario_X_X_0_wins(self):
+        noughts_crosses_array = ["X", "X", "0", "", "", "", "", "", ""]
+        self.assertFalse(self.test_target.someone_has_won(noughts_crosses_array))
 
 
 if __name__ == '__main__':
